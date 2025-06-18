61c8346c53249401df2c981c0d0fe1f27af99eec
Antonio Augusto de Moura Neto <160650423+AugustoNeto00@users.noreply.github.com>
2025-06-18 18:24:19 -0300

    O registro está funcionando perfeitamente, com validação de senha, o css da pagina esta feito e a confirmação no banco de dados está ok.

--- app.js
+++ app.js
@@ -43,48 +43,19 @@ app.get('/', (req, res) => {
 });
 
 // Rota para a página de registro
-app.get('/register-page.ejs', (req, res) => {
-    res.render('register-page', { title: 'Cadastro'}); // Renderiza a página 'register-page.handlebars'
+app.get('/register-page', (req, res) => {
+    res.render('register-page.ejs', { title: 'Cadastro'}); // Renderiza a página 'register-page.handlebars'
 });
-app.get('/complaint-page.ejs', (req, res) => {
-    res.render('complaint-page', { title: 'Página de denuncia'}); // Renderiza a página 'complaint-page.handlebars'
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/home.ejs');
+app.get('/complaint-page', (req, res) => {
+    res.render('complaint-page.ejs', { title: 'Página de denuncia'}); // Renderiza a página 'complaint-page.handlebars'
 });
 
-app.get('/', (req, res) => {
-    res.render('register-page', { title: 'Cadastro'});
-});
-
-app.get('/ini', (req, res) => {
-    res.render('home')
+app.get('/home', (req, res) => {
+    res.render('home.ejs')
 })
 
-app.post('/register-page.ejs', async (req, res) =>{
-    const data = {
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    }
-    //checkando se usuario ja existe
-    const exitingUser = await collection.findOne({name: data.name}); //faz a varredura da coleção
-    if(exitingUser){
-        return res.send('User already exist');
-    }else {
-    //hashing the password
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(data.password, saltRounds);
 
-    data.password = hashedPassword;
 
-    const userData = await collection.insertMany(data);
-    console.log(userData);  
-
-    res.redirect('/complaint-page.ejs');
-    }
-});
 app.post('/complaint-page.ejs', async (req, res) =>{
     const data = {
         whichComplaint: req.body.whichComplaint,
@@ -108,3 +79,36 @@ const port = 8080;
 app.listen(port, () => {
     console.log('Servidor iniciado na porta: ' + port);
 });
+app.post('/register-page', async (req, res) =>{
+    try{
+        const data = {
+            email: req.body.email,
+            password: req.body.password,
+            passwordAgain: req.body.passwordAgain
+        }
+        if ( req.body.password != req.body.passwordAgain){
+            res.send(`<script>
+                alert("As senhas não coincidem");
+                window.location.href = "/register-page"; // Redireciona após o alerta
+            </script>`)
+        } else {
+        //hashing the password
+            const saltRounds = 5;
+            const hashedPassword = await bcrypt.hash(data.password, saltRounds);
+
+            data.password = hashedPassword;
+
+            const userData = await collection.insertOne({
+                email: data.email,
+                password: hashedPassword
+            });
+            console.log(userData);
+
+            res.redirect('/complaint-page');
+        }
+    }catch (error) {
+        console.error("Erro ao conectar o usuário: ", error);
+        res.status(500).send("Erro interno no servidor")
+    }
+    
+}); 
--- views/assets/js/index.js
+++ views/assets/js/index.js
@@ -1,4 +1,4 @@
 function redirecionar(){
-    window.location = "/register-page.ejs"
+    window.location.href = "/register-page.ejs"
     
 }
